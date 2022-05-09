46da1862b265a11223edc619ff03116ee41b5d95
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-05-09 10:58:39 -0700

    Added the start of the javascript that will be needed to calculate scores. All very simple. Only set up practice scrpts for the Dex and acrobatics as well as proficiency modifier.

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-let farts = "toot toot"
+//============
+// FUNCTIONS
+//============
+
+//Determine the proficiency bonus based off of character level
+function getProficiencyBonus(level) {
+    
+    if (level < 1 || level > 20) {
+        return false;
+    } else if(level <= 4) {
+        return 2;
+    } else if (level >= 5 && level <= 8) {
+        return 3;
+    } else if (level >= 9 && level <= 12) {
+        return 4;
+    } else if (level >= 13 && level <= 16) {
+        return 5;
+    } else {
+        return 6;
+    }
+}
+
+function getAttributeMod(score) {
+    if (score === 10) {
+        return 0;
+    } else if (score > 10) {
+        return Math.floor((score - 10)/2);
+    } else {
+        return Math.floor(-((10 - score)/2));
+    }
+}
+
+function calculate() {
+    const skill = document.querySelector('#acrobatics');
+    let attribute = document.querySelector('.dexterity').value;
+    let level = document.querySelector('.charlvl').value;
+    let result = document.querySelector('.acrobaticsResult');
+
+
+    if (skill.checked) {
+        result.textContent = getAttributeMod(attribute) + getProficiencyBonus(level);
+    } else {
+        result.textContent = getAttributeMod(attribute);
+    }
+
+}
